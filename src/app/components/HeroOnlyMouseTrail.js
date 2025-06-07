2b7b9315eeb9a81ae90ce37c1d6f39c872e4d793
SamuelPerezTorres <spertor@epsa.upv.es>
2025-06-07 16:52:54 +0200

    Fix ESLint error: remove unused useRef import

--- src/app/components/HeroOnlyMouseTrail.js
+++ src/app/components/HeroOnlyMouseTrail.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import MouseTrail from './MouseTrail';
 
 export default function HeroOnlyMouseTrail() {
